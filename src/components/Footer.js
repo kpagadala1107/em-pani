fa5d83c26f80835a4886306a432e2020eba2ff53
kprashanthi023 <kprashanthi023@gmail.com>
2024-10-24 21:42:36 -0500

    test change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <p>&copy; 2024 Freelance Marketplace. All rights reserved.</p>
+      <p>&copy; 2025 Freelance Marketplace. All rights reserved.</p>
     </footer>
   );
 }
